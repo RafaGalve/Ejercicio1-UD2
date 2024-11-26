9e292e7917227a6db8301410362b349515159419
Rafa <24.damt.rafaelenriquegalve@fundacionmontessori.com>
2024-11-26 20:53:11 +0100

    Añado Proyecto1 de Java

---
+++ Proyecto1/src/mostrandodatos/MostrandoDatos.java
@@ -0,0 +1,21 @@
+
+package mostrandodatos;
+
+import java.util.Scanner;
+
+public class MostrandoDatos {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Introduce tu nombre: ");
+		String nombre = entrada.nextLine();
+		
+		System.out.print("Introduce tu apellido: ");
+		String apellido = entrada.nextLine();
+		
+		System.out.println("Nombre completo: " + nombre + " " + apellido);
+		
+		entrada.close();
+	}
+
+}
